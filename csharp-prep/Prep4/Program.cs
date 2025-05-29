56d3d63d558e996523cbc1539c8d7c7899fa224f
Ty Miller <tyghvm19@byui.edu>
2025-05-28 21:45:45 -0600

    Finished number crunching assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,35 @@
 using System;
-
+//This program demonstrates how lists are made, used and accesed
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<float> numbers = new List<float>(); //We use parentheses whenever we make a new object with list being the class
+        float floatNum;
+        float sum = 0;
+        float average;
+        float maxNum;
+
+        do 
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished: ");
+            string strNumber = Console.ReadLine();
+            floatNum = float.Parse(strNumber);
+            if (floatNum != 0)
+                numbers.Add(floatNum);
+                sum += floatNum;
+        } 
+        while (floatNum != 0);
+
+        //Compute sum
+        Console.WriteLine($"The sum is: {sum}"); 
+        //Compute the average
+        Console.WriteLine($"The average is: {average = sum/numbers.Count}");
+        //Find largest number
+        Console.WriteLine($"The largest is: {maxNum = numbers.Max()}");
+
+
+
+
     }
 }
